526307fcbb2cc4144151b7c186604a08da57f56b
MatteoCodeSmith <matteo.mirafiori@gmail.com>
2024-04-06 11:43:37 +0200

    Initial commit 06Apr2024

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# DokerizedAPI-ReactMongo
+# Dokerized Rest API using React.js and MongoDB
+
+In this application, an initial state has been set to develop a Rest API using React.js and MangoDB, all included in Docker containers. 
+
+This repository therefore allows you to have a working API to start developing an ecommerce or any other API application.
+
+- The first step to get started is to create the .env file:
+
+      cp .env.sample .env
+
+- Then use the following command to start the application:
+
+      docker compose up -d
+
+- To check that the application is working, go to the following website
+
+      http://0.0.0.0:${NODE_LOCAL_PORT}/api/test
+
+      NODE_LOCAL_PORT -> is inside the .env file.
+
+
+The application updates the code inside the container in real time, allowing you to have a development environment that updates React every time the code changes.
+
+Have fun with this new tool! =)
---
+++ crafting_api/server.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+
+console.log(db.url);
+
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
+// Simple example to get function 
+// To check the website you have to go in the following website: http://0.0.0.0:${NODE_LOCAL_PORT}/api/test -> NODE_LOCAL_PORT is inside the .env file
+app.get("/api/test", (req, res) => {
+  res.json({ message: "First example of an API dockerized using React and MongoDB." });
+  console.log("First API Rest request!!")
+});
+
+
+// set port, listen for requests
+const PORT = process.env.NODE_DOCKER_PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
